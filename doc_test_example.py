70f00554c841ae8f1384a559bf197b6c18191530
myamashita <marciokatsumiyamashita@gmail.com>
2020-09-21 14:42:21 -0500

    Update doc_test_example.py

--- doc_test_example.py
+++ doc_test_example.py
@@ -4,14 +4,14 @@ This is the "example" module.
 The example module supplies one function, factorial().  For example,
 
 >>> factorial(5)
-120
+125
 """
 
 def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
 
     >>> [factorial(n) for n in range(6)]
-    [1, 1, 2, 6, 24, 122]
+    [1, 1, 2, 6, 24, 120]
     >>> factorial(30)
     265252859812191058636308480000000
     >>> factorial(-1)
